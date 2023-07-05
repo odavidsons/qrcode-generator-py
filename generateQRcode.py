3dd664bc233718092787feebce2398d659c74f19
David Santos <davidsantos2807@gmail.com>
2023-07-05 11:02:02 +0100

    Added customization options

--- generateQRcode.py
+++ generateQRcode.py
@@ -1,6 +1,6 @@
 import qrcode
 import customtkinter as ctk
-from PIL import ImageTk
+from PIL import ImageTk,Image
 
 class generateQRcode(ctk.CTk):
 
@@ -17,26 +17,50 @@ class generateQRcode(ctk.CTk):
         frameLeft.grid(row=0,column=0,sticky="nsew",padx=5,pady=10)
         self.tabView = ctk.CTkTabview(frameLeft)
         self.tabView.grid(row=0,column=0,sticky="nsew")
-        self.tabView.add("From Text")
-        self.tabView.add("From Image")
-        self.tabView.set("From Text")
-        label1 = ctk.CTkLabel(self.tabView.tab("From Text"),text="Enter text:",font=self.fontMedium)
+        self.tabView.add("Input data")
+        self.tabView.add("Configurations")
+        self.tabView.set("Input data")
+        label1 = ctk.CTkLabel(self.tabView.tab("Input data"),text="Enter text:",font=self.fontMedium)
         label1.grid(row=0,column=0,padx=20)
-        self.inputText = ctk.CTkTextbox(self.tabView.tab("From Text"))
+        self.inputText = ctk.CTkTextbox(self.tabView.tab("Input data"))
         self.inputText.grid(row=1,column=0,padx=20,pady=10)
-        label2 = ctk.CTkLabel(self.tabView.tab("From Image"),text="Select image:",font=self.fontMedium)
-        label2.grid(row=0,column=0,padx=20)
-        btnSelectImg = ctk.CTkButton(self.tabView.tab("From Image"),text="Select",font=self.fontMedium,command=self.selectImage)
-        btnSelectImg.grid(row=1,column=0,padx=20,pady=10)
-        self.labelImgPath = ctk.CTkLabel(self.tabView.tab("From Image"),text="Path:")
-        self.labelImgPath.grid(row=2,column=0,padx=20,pady=5)
+        label2 = ctk.CTkLabel(self.tabView.tab("Configurations"),text="Foreground color:")
+        label2.grid(row=0,column=0,padx=10)
+        colors = ["darkred","red","orange","yellow","green","lime","cyan","blue","purple","violet","pink","white","black","brown"]
+        self.foregroundColor = ctk.StringVar()
+        self.foregroundColor.set("black")
+        select1 = ctk.CTkOptionMenu(self.tabView.tab("Configurations"),variable=self.foregroundColor,values=colors)
+        select1.grid(row=0,column=1,padx=10,pady=10)
+        label3 = ctk.CTkLabel(self.tabView.tab("Configurations"),text="Background color:")
+        label3.grid(row=1,column=0,padx=10)
+        self.backgroundColor = ctk.StringVar()
+        self.backgroundColor.set("white")
+        select2 = ctk.CTkOptionMenu(self.tabView.tab("Configurations"),variable=self.backgroundColor,values=colors)
+        select2.grid(row=1,column=1,padx=10,pady=10)
+        sizes = ["1","2","3","4","5","6","7","8","9","10"]
+        self.qrcodeSize = ctk.StringVar()
+        self.qrcodeSize.set("1")
+        label4 = ctk.CTkLabel(self.tabView.tab("Configurations"),text="Size:")
+        label4.grid(row=2,column=0,padx=10)
+        select3 = ctk.CTkOptionMenu(self.tabView.tab("Configurations"),variable=self.qrcodeSize,values=sizes)
+        select3.grid(row=2,column=1,padx=10,pady=10)
+        label4 = ctk.CTkLabel(self.tabView.tab("Configurations"),text="Logo image:")
+        label4.grid(row=3,column=0,padx=10)
+        btnSelectImg = ctk.CTkButton(self.tabView.tab("Configurations"),text="Select",command=self.selectImage)
+        btnSelectImg.grid(row=3,column=1,padx=10,pady=10)
+        self.labelImgPath = ctk.CTkLabel(self.tabView.tab("Configurations"),text="Path:")
+        self.labelImgPath.grid(row=4,column=0,padx=20,pady=5)
 
         frameCenter = ctk.CTkFrame(self)
         frameCenter.grid(row=0,column=1,padx=5,pady=10)
-        btnGenerate = ctk.CTkButton(frameCenter,text=">> Generate >>",font=self.fontMedium,command=self.setType)
+        btnGenerate = ctk.CTkButton(frameCenter,text=">> Generate >>",font=self.fontMedium,command=self.generateCode)
         btnGenerate.grid(row=0,column=0,padx=20,pady=20,sticky="ns")
         btnClear = ctk.CTkButton(frameCenter,text="Clear QR Code",font=self.fontMedium,command=self.clearCode)
         btnClear.grid(row=1,column=0,padx=20,pady=20,sticky="ns")
+        btnSave = ctk.CTkButton(frameCenter,text="Save as",font=self.fontMedium,command=self.saveCode)
+        btnSave.grid(row=2,column=0,padx=20,pady=20,sticky="ns")
+        self.labelMessage = ctk.CTkLabel(frameCenter,text="")
+        self.labelMessage.grid(row=3,column=0,padx=10,pady=5)
 
         frameRight = ctk.CTkFrame(self)
         frameRight.grid(row=0,column=2,sticky="nsew",padx=5,pady=10)
@@ -48,28 +72,34 @@ class generateQRcode(ctk.CTk):
         self.imagePath = ctk.filedialog.askopenfilename(filetypes=filetypes,title="Select an image")
         self.labelImgPath.configure(text=f"Path: {self.imagePath}")
 
-    def setType(self):
-        text = self.inputText.get("1.0", "end-1c")
-        image = self.imagePath
-        selectedTab = self.tabView.get()
-        if selectedTab == "From Text":
-            self.generateCode(text)
-        elif selectedTab == "From Image":
-            self.generateCode(image)
-
-    def generateCode(self,data):
-        if len(data) != 0:
+    def generateCode(self):
             try:
-                generated = qrcode.make(data)
-                img = ImageTk.PhotoImage(generated)
-                #PIL = Image.open("qrcode.png")
-                #img = ctk.CTkImage(dark_image=generated,size=(self.winfo_height(),self.winfo_height()))
-                self.labelQRCode.configure(image=img)
-            except: pass
-        else: print("Data is empty")
+                self.labelMessage.configure(text="")
+                text = self.inputText.get("1.0", "end-1c")
+                if self.imagePath != "":
+                    logo = Image.open(self.imagePath)
+                if len(text)>0:
+                    qr = qrcode.QRCode(version=self.qrcodeSize.get(),error_correction=qrcode.ERROR_CORRECT_L,box_size=10,border=4)
+                    qr.add_data(text)
+                    qr.make(fit=True)
+                    generated = qr.make_image(fill_color=self.foregroundColor.get(), back_color=self.backgroundColor.get())
+                    qrcode_img = ImageTk.PhotoImage(generated)
+                    self.labelQRCode.configure(image=qrcode_img)
+                    self.labelMessage.configure(text="Code generated!")
+                else: self.labelMessage.configure(text="You can't create an empty QR code.")
+            except: self.labelMessage.configure(text="There was an error generating your QR code.")
 
     def clearCode(self):
         self.labelQRCode.configure(image=None)
+        self.labelMessage.configure(text="")
+
+    def saveCode(self):
+        if self.labelQRCode.cget("image") != None:
+            filetypes = (('Image Files', '.png .jpeg .jpg'),('All files', '*.*'))
+            filename = ctk.filedialog.asksaveasfilename(filetypes=filetypes,title="Save image as")
+            img = ImageTk.getimage(self.labelQRCode.cget("image"))
+            img.save(filename)
+            self.labelMessage.configure(text="Image saved!")
 
 windowApp = generateQRcode()
 windowApp.title("QR Code Generator")
